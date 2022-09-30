7205e86a87841ee910cc95b9d7f4817b9db84c83
ldsleticia <lds.leticia.dos.santos@gmail.com>
2022-09-29 22:55:06 -0300

    exercicio c capitulo 2

--- capitulo-2/exercicio-c/script.js
+++ capitulo-2/exercicio-c/script.js
@@ -1,10 +1,12 @@
-function enviar() {
-  let durationValue = document.getElementById("inFilmDuration").value;
-  let filmName = document.getElementById("inFilmName").value;
-  let transformInHours = (durationValue / 60).toFixed(2);
-  let answer = (document.getElementById(
-    "outNameAndDuration"
-  ).textContent = `O filme '${filmName}' tem duração de ${Number(
-    transformInHours
-  )} hora(s)`);
+function send() {
+  const durationValue = document.getElementById("inFilmDuration").value;
+  const filmName = document.getElementById("inFilmName").value;
+
+  const hours = Math.floor(durationValue / 60);
+  const minutes = durationValue % 60;
+  const hoursText = `00${hours}`.slice(-2);
+  const minutesText = `00${minutes}`.slice(-2);
+
+
+  const answer = document.getElementById('outNameAndDuration').textContent = `O filme '${filmName}' tem duração de ${hoursText} hora(s) e ${minutesText} minutos`
 }
